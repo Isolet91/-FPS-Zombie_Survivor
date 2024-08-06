8fd84f5dae87761aec05844a50617b442405df0c
Isolet91 <psn08217@naver.com>
2024-08-06 14:11:06 +0900

    8/6 LivingEntity / UIManager

--- Assets/Scripts/LivingEntity.cs
+++ Assets/Scripts/LivingEntity.cs
@@ -10,6 +10,7 @@ public class LivingEntity : MonoBehaviour, IDamageable
     public float startingHealth = 100f;         // 시작 체력
     public float health { get; protected set; } // 현재 체력
     public bool dead { get; protected set; }    // 사망 상태
+    
     public event Action onDeath;                // 사망시 발동할 이벤트
 
     // 생명체가 활성화될때 상태를 리셋
